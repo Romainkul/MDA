3564c63197ff9c5098a9139a4aa0af3141e036ed
rom1ber <romain.berard007@gmail.com>
2025-05-23 16:01:54 +0200

    e

--- backend/main.py
+++ backend/main.py
@@ -435,7 +435,7 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
 
     # Create your text-generation pipeline on CPU
     gen_pipe = pipeline(
-        "text2text-generationr",#"text-generation",
+        "text2text-generation",#"text-generation",
         model=llm_model,
         tokenizer=AutoTokenizer.from_pretrained(settings.llm_model),#, use_fast=False),
         device=-1,              # force CPU
