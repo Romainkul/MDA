d71bc40102e433d6bba12c42a8b7df92bac27034
rom1ber <romain.berard007@gmail.com>
2025-05-24 21:05:49 +0200

    Fix dates

--- frontend/src/components/ProjectExplorer.tsx
+++ frontend/src/components/ProjectExplorer.tsx
@@ -241,7 +241,7 @@ const ProjectExplorer: React.FC<ProjectExplorerProps> = ({
                     <Td w="50%" overflow="hidden" textOverflow="ellipsis">{p.title}</Td>
                     <Td w="10%">{p.status}</Td>
                     <Td w="10%">{p.id}</Td>
-                    <Td w="10%" whiteSpace="nowrap">{p.startDate ? p.startDate.slice(0, 10) : "—"}</Td>
+                    <Td w="10%" whiteSpace="nowrap">{p.startDate ? new Date(p.startDate).toLocaleDateString('en-CA')  : "—"}</Td>
                     <Td w="10%">{p.fundingScheme || '-'}</Td>
                     <Td w="10%">€{fmtNum(p.ecMaxContribution)}</Td>
 
